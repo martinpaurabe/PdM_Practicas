dc4cb6003d14ee3ce50cbe6cf6b20b93843ad26e
martin_paura_bersan <mpaurabe@itba.edu.ar>
2023-11-29 23:35:34 -0300

    Con funciones de lectura de Unit_ComPort

--- FinalProject/PC_Simulator/Drivers/Inc/Unit_ThreadComPort.h
+++ FinalProject/PC_Simulator/Drivers/Inc/Unit_ThreadComPort.h
@@ -1,16 +1,9 @@
 #ifndef Unit_ThreadComPortH
 #define Unit_ThreadComPortH
 //---------------------------------------------------------------------------
+//My Libraries
+#include "Drivers/Inc/Unit_ComPort.h"
 
-// C library headers
-#include <stdio.h>
-#include <string.h>
-
-// Linux headers
-#include <fcntl.h> // Contains file controls like O_RDWR
-#include <errno.h> // Error integer and strerror() function
-#include <termios.h> // Contains POSIX terminal control definitions
-#include <unistd.h> // write(), read(), close()
 
 //---------------------------------------------------------------------------
 
--- FinalProject/PC_Simulator/main.cpp
+++ FinalProject/PC_Simulator/main.cpp
@@ -3,22 +3,75 @@
 
 
 //My Libraries
-#include "Drivers/Inc/Unit_ThreadComPort.h"
+//#include "Drivers/Inc/Unit_ThreadComPort.h"
+#include "Drivers/Unit_ComPort.h"
 
 
 using namespace std;
 
 int main()
 {
-    int serial_port = open("/dev/ttyACM0", O_RDWR);
+  OpenCommPort(115200);
+  // Write to serial port
+ // unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r' };
+  
 
-  // Create new termios struct, we call it 'tty' for convent2ion
-  struct termios tty;
+  // Allocate memory for read buffer, set size according to your needs
+  char read_buf [256];
+
+  // Normally you wouldn't do this memset() call, but since we will just receive
+  // ASCII data for this example, we'll set everything to 0 so we can
+  // call printf() easily.
+  memset(&read_buf, '\0', sizeof(read_buf));
+  int num_bytes=0;
+
+  while(1)
+  {
+   // Read bytes. The behaviour of read() (e.g. does it block?,
+    // how long does it block for?) depends on the configuration
+    // settings above, specifically VMIN and VTIME
+    num_bytes = BytesDisponibles(); 
+  
+       // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
+    if (num_bytes < 0) {
+      printf("Error reading: %s", strerror(errno));
+      return 1;
+    }
+
+    // Here we assume we received ASCII data, but you might be sending raw bytes (in that case, don't try and
+    // print it to the screen like this!)
+    
+    if(10< num_bytes)
+    {
+      ReadBytes(read_buf,9);
+      printf("%s\r\n", read_buf);
+    }
+ 
+  //cout << "Hello world!" << endl;
+  }  
+  
+  return 0; // success
+};
+
+
+
+//---------------------------------------------------------------------------
+//My Private Variables
+// Create new termios struct, we call it 'tty' for convent2ion
+static struct termios tty;
+static int32_t serial_port = 0;
+static bool m_CommOpen = false;
+
+int32_t OpenCommPort(unsigned int Baudios)
+{
+  serial_port = open("/dev/ttyACM0", O_RDWR);
+
+  
 
   // Read in existing settings, and handle any error
   if(tcgetattr(serial_port, &tty) != 0) {
-      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-      return 1;
+    printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    return 1;
   }
 
   tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
@@ -53,41 +106,164 @@ int main()
     printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
     return 1;
   }
-
-  // Write to serial port
-  unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r' };
-  write(serial_port, msg, sizeof(msg));
-
-  // Allocate memory for read buffer, set size according to your needs
-  char read_buf [256];
-
-  // Normally you wouldn't do this memset() call, but since we will just receive
-  // ASCII data for this example, we'll set everything to 0 so we can
-  // call printf() easily.
-  memset(&read_buf, '\0', sizeof(read_buf));
-  int num_bytes=0;
-
-  while(num_bytes<250)
-  {
-   // Read bytes. The behaviour of read() (e.g. does it block?,
-    // how long does it block for?) depends on the configuration
-    // settings above, specifically VMIN and VTIME
-    num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
   
-       // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
-    if (num_bytes < 0) {
-      printf("Error reading: %s", strerror(errno));
-      return 1;
+  m_CommOpen = true;
+  return 0;
+}
+//---------------------------------------------------------------------------
+
+
+void CloseCommPort(void)
+{
+    if(!serial_port)       // if already closed, return
+      return;
+
+    if(close(serial_port) != 0) // CloseHandle is non-zero on success
+      m_CommOpen = false;
+    else
+      printf("Error, no se pudo cerrar el puerto");
+}
+//---------------------------------------------------------------------------
+
+
+bool ComAbierto(void)
+{
+  return m_CommOpen;
+}
+//---------------------------------------------------------------------------
+
+
+int32_t ComNum(void)
+{
+  return serial_port;
+}
+//---------------------------------------------------------------------------
+
+/*
+void SetBaudRate(uint32_t baud)
+{
+    unsigned int oldBaudRate = m_dcb.BaudRate; // backup
+    m_dcb.BaudRate = baud;
+
+    if(m_CommOpen) //Si el puerto est� abierto actualizar baud rate
+    {
+        if(!SetCommState(m_hCom,&m_dcb))   // trato de setear
+        {                                      // si no
+            m_dcb.BaudRate = oldBaudRate;        // vuelvo al viejo B.rate
+            ShowMessage("Error no se pudo cambiar el BaudRate");
+        }
+    }
+}
+*/
+//---------------------------------------------------------------------------
+
+
+int32_t BytesDisponibles(void)
+{
+  int32_t bytes_avaiable;
+  ;
+    if(-1 == ioctl(serial_port, FIONREAD, &bytes_avaiable))
+      return -1;
+
+    return bytes_avaiable; //Return bytes in input buffer
+}
+//---------------------------------------------------------------------------
+
+
+DWORD ComError(void)
+{
+   // COMSTAT Status;
+    DWORD Errores=0;
+
+ //   if(!ClearCommError(m_hCom, &Errores, &Status))
+ //     return -1;
+
+    return Errores;
+}
+//---------------------------------------------------------------------------
+
+
+int32_t GetByte(BYTE *value)
+{
+    unsigned long BytesRead;
+
+    if (!m_CommOpen)
+      return 0;
+
+    if(!read(serial_port, value, 1))
+      return -1;
+
+    return BytesRead;
+}
+//---------------------------------------------------------------------------
+
+
+int32_t ReadBytes(void *Buffer, int n)
+{
+    unsigned long BytesRead;
+
+    if (!m_CommOpen) //Verifica si el puerto esta abierto
+      return 0;
+
+    if(!read(serial_port, Buffer, n))
+      return -1;
+
+    return BytesRead;
+}
+//---------------------------------------------------------------------------
+
+
+int32_t PutByte(BYTE value)
+{
+    unsigned long BytesWritten;
+
+    if (!m_CommOpen)
+      return 0;
+
+    if(!write(serial_port,  &value, 1))
+      return -1;
+
+    return BytesWritten;
+}
+//---------------------------------------------------------------------------
+
+
+int32_t WriteBytes(void *Buffer, int n)
+{
+    unsigned long BytesWritten;
+
+    if (!m_CommOpen) //Verifica si el puerto esta abierto
+      return 0;
+    if(!write(serial_port, Buffer, n))
+      return -1;
+
+    return BytesWritten;
+}
+//---------------------------------------------------------------------------
+
+bool sendSciMsg(BYTE codigo, void *data, DWORD dataLen)
+{
+    DWORD cantWritten = 0;
+
+    if(!m_CommOpen) //Verifica si el puerto esta abierto
+      return false;
+
+    if((data == NULL) && dataLen)
+      return false;
+
+    cantWritten += PutByte(SFD);
+    cantWritten += PutByte(dataLen+1);
+    cantWritten += PutByte(codigo);
+    if(!write(serial_port, data, dataLen))
+      return false;
+    cantWritten += PutByte(EOF);
+
+    if(cantWritten < (4+dataLen))
+    {
+      return false;
     }
 
-    // Here we assume we received ASCII data, but you might be sending raw bytes (in that case, don't try and
-    // print it to the screen like this!)
-    
-    if(0< num_bytes)
-        printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
+    return true;
+}
+//---------------------------------------------------------------------------
 
-  //cout << "Hello world!" << endl;
-  }  
-  close(serial_port);
-  return 0; // success
-};
